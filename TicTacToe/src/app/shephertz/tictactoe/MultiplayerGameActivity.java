3f69ab6e3de93555406deeda8d1ba022ffd6f1a5
SauravGShephertz <saurav.gulati@shephertz.co.in>
2014-03-12 13:11:45 +0530

    init TicTacToe

---
+++ TicTacToe/src/app/shephertz/tictactoe/MultiplayerGameActivity.java
@@ -0,0 +1,232 @@
+package app.shephertz.tictactoe;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import app.shephertz.multiplayer.WarpController;
+
+public class MultiplayerGameActivity extends Activity implements OnTouchListener{
+	
+	RelativeLayout grid_view;
+	private char[][] ARRAY = new char[3][3];
+	private Random random = new Random();
+	
+	private final int GRID_WIDTH = 256;
+	private final int OBJECT_WIDTH = 64;
+	private int GAP = 256/3;
+	private char TYPE = '0';
+	
+	private boolean isGameStarted = false;
+	private boolean isUserTurn = false;
+	
+	private TextView notificationTextView;
+	private TextView resultTextView;
+		
+	private void init(){
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				ARRAY[i][j] = '-';
+			}
+		}
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game);
+		grid_view = (RelativeLayout)findViewById(R.id.grid_layout);
+		notificationTextView = (TextView)findViewById(R.id.notification_text_view);
+		resultTextView = (TextView)findViewById(R.id.result_text_view);
+		grid_view.setOnTouchListener(this);
+		notificationTextView.setText("Please wait...");
+		init();
+		connectToAppWarp();
+		GAP = Util.dpToPx(getApplicationContext(), GAP);
+	}
+	
+	private void connectToAppWarp(){
+		WarpController.getInstance().setActivity(this);
+		Util.UserName = Util.getRandomHexString(10);
+		WarpController.getInstance().startApp(Util.UserName, this);
+	}
+	
+	public void setType(char t){
+		this.TYPE = t;
+	}
+	
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		if(isGameStarted && isUserTurn){
+			getIndex(event.getX(), event.getY());
+		}
+		return false;
+	}
+	
+	private void updateUI(int i, int j, char type){ 
+	  ARRAY[i][j] = type;
+	  int GAP = 256/3;
+	  int DRAW_X = (j*GAP) + GAP/2 - OBJECT_WIDTH/2;
+	  int DRAW_Y = (i*GAP) + GAP/2 - OBJECT_WIDTH/2;
+	  ImageView imgView = new ImageView(this);
+	  
+	  if( type == '0' ) {
+		  imgView.setImageDrawable(getResources().getDrawable(R.drawable.button_ai));
+	  }else if ( type == 'X' ){
+		  imgView.setImageDrawable(getResources().getDrawable(R.drawable.button_exit));
+	  }
+	    
+	  RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams((LayoutParams.WRAP_CONTENT), (LayoutParams.WRAP_CONTENT));
+	  DRAW_X = Util.dpToPx(getApplicationContext(), DRAW_X);
+	  DRAW_Y = Util.dpToPx(getApplicationContext(), DRAW_Y);
+	  lp.leftMargin = DRAW_X;
+	  lp.topMargin = DRAW_Y;
+	  imgView.setLayoutParams(lp);
+	  grid_view.addView(imgView);
+	  
+	}
+	
+	private void getIndex(float touchX, float touchY){
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				if ( touchX>(j*GAP) && touchX<((j*GAP)+GAP) && touchY>((i*GAP)) && touchY<(i*GAP)+GAP ) {
+					if(ARRAY[i][j] == '-' && TYPE=='0') {
+		              ARRAY[i][j] = '0';
+		              updateUI(i, j, TYPE);
+		            }
+		            else if(ARRAY[i][j] == '-' && TYPE=='X') {
+		              ARRAY[i][j] = 'X' ; 
+		              updateUI(i, j, TYPE);
+		            }
+					checkGameComplete(i, j);
+					
+				}
+			}
+		}
+	}
+	
+	private void checkGameComplete(int i, int j){
+		char r = Util.checkForWin(ARRAY);
+		if(r!='-'){
+			if(r==TYPE){
+				showResultDialog("Congrats, You Win");
+				WarpController.getInstance().sendMove(i+"#"+j+"/"+"Win");
+				return;
+			}else{
+				showResultDialog("Oops, You Loose");  
+				WarpController.getInstance().sendMove(i+"#"+j+"/"+"Loose");
+				return;
+			}
+		}
+		WarpController.getInstance().sendMove(i+"#"+j+"/"+"");
+	}
+	
+	private void showResultDialog(String message){
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(message)
+		       .setCancelable(false)
+		       .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+		        	    WarpController.getInstance().stopApp();
+		                MultiplayerGameActivity.this.finish();
+		           }
+		       });
+		AlertDialog alert = builder.create();
+		alert.show();
+		
+	}
+	
+	public void updateNotification(final String text){
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				notificationTextView.setText(text);
+			}
+		});
+	}
+	
+	public void startGame(final String text){
+		isGameStarted = true;
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				if(text.equals(Util.UserName)){
+					isUserTurn = true;
+					notificationTextView.setText(text);
+					notificationTextView.setText("Your Turn");
+				}else{
+					isUserTurn = false;
+					notificationTextView.setText("Enemy Turn");
+				}
+			}
+		});
+	}
+	
+	@Override
+	public void onBackPressed() {
+		WarpController.getInstance().stopApp();
+		super.onBackPressed();
+	}
+	
+	public void onMoveCompleted(String moveData, final String nextTurn){
+		final int i = Integer.parseInt(moveData.substring(0,moveData.indexOf('#')));
+		final int j = Integer.parseInt(moveData.substring(moveData.indexOf('#')+1, moveData.indexOf('/')));
+		final String data = moveData.substring(moveData.indexOf('#')+1, moveData.length());
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				if(nextTurn.equals(Util.UserName)){
+					isUserTurn = true;
+					notificationTextView.setText("Your Turn");
+				}else{
+					isUserTurn = false;
+					notificationTextView.setText("Enemy Turn");
+				}
+				if(TYPE=='0'){
+					updateUI(i, j, 'X');
+				}else{
+					updateUI(i, j, '0');
+				}
+				if(data.trim().length()>0){
+					if(data.indexOf("Win")!=-1){
+						isGameStarted = false;
+						showResultDialog("Oops! You Loose");
+					}
+					if(data.indexOf("Loose")!=-1){
+						isGameStarted = false;
+						showResultDialog("Congrats! You Win");
+					}
+				}
+			}
+		});
+	}
+	
+	public void onEnemyLeft(){
+		if(isGameStarted){
+			runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					showResultDialog("Congrats! You Win\nEnemy Left");
+				}
+			});
+		}
+		
+	}
+	
+	
+}
